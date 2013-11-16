6eba12d52d5a46f7f0362a8925caf2db600ef977
Brian Lange <thebrianlange@gmail.com>
2013-11-16 13:31:26 -0600

    Add item parsing to spider

--- reddit_scraper/reddit_scraper/spiders/post_spider.py
+++ reddit_scraper/reddit_scraper/spiders/post_spider.py
@@ -1,4 +1,5 @@
 from scrapy.spider import BaseSpider
+from reddit_scraper.items import RedditScraperItem
 import simplejson as json
 import pprint
 pp = pprint.PrettyPrinter()
@@ -24,11 +25,27 @@ class PostSpider(BaseSpider):
 
     def parse(self, response):
         raw_data = json.loads(response.body)
+        items = []
         for story in raw_data['data']['children']:
-            print story['data']['id'], story['data']['title']
 
+            item = RedditScraperItem()
+            item["title"] = story['data']['title']
+            item["permalink"] = story['data']['permalink']
+            item["post_id"] = story['data']['id']
+            item["creation_time"] = story['data']['created_utc']
+            item["domain"] = story['data']['domain']
+            item["author"] = story['data']['author']
+            item["subreddit"] = story['data']['subreddit']
+
+            # what should we use for submission type? media_embed?
+            # item["submission_type"] = story['data']['']
+            
+            items.append(item)
+        
         # id of the last post received, for forming the next url like so:
         # http://reddit.com/r/doge/new.json?after=t3_1qluhk
         print raw_data['data']['after']
+
+        return items
 
         pass
