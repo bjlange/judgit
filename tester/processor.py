0daa5ac304fe0b363a2888ad7715633d75936696
wesleyks <sunwes@gmail.com>
2013-12-07 14:51:29 -0600

    added more informative stuff

--- tester/processor.py
+++ tester/processor.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 	print 'starting'
 	currentFold = 1
 	for train, test in kf:
+		print 'on fold %s' % (currentFold)
 		trainingIndices = train
 		testingIndices = test
 		trainingData = [postArray[i] for i in trainingIndices]
