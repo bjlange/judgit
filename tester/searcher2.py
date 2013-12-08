ce616f982fd84dc1a64bfa6ad29be437dacea1d5
wesleyks <sunwes@gmail.com>
2013-12-07 20:45:13 -0600

    okay we gonna go with this

--- tester/searcher2.py
+++ tester/searcher2.py
@@ -9,6 +9,16 @@ from heapq import nsmallest
 def weightedSum(weights, values):
 	return sum(map(lambda x,y: x*y, weights, values))
 
+def generateWeights():
+	weights = []
+	for i in range(6):
+		for val in [0.0, .25, .5, 2.0, 4.0]:
+			basic = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+			basic[i] = val
+			weights.append(basic)
+	weights.append([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+	return weights
+
 if __name__ == '__main__':
 	path = os.path.join('data', 'fold1')
 	files = [f for f in os.listdir(path) if os.path.isfile(f)]
@@ -33,18 +43,21 @@ if __name__ == '__main__':
 
 	print 'starting'
 
-	weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+	weights = generateWeights()
 
-	sse = 0.0
+	sseArray = [0.0] * len(weights)
 	for post in postArray:
 		path = os.path.join('data', 'fold1', post['id'])
 		with open(path, 'r') as f:
 			allDistances = [[float(x) for x in line.split(',')] for line in f.read().split('\n') if len(line) > 0]
-			items = nsmallest(1, allDistances, key=lambda x: weightedSum(weights, x[0:6]))
-			scores = map(lambda x: x[6], items)
-			score = score / len(items)
-			sse += (score - post['realScore']) ** 2
+			for i in range(len(weights)):
+				items = nsmallest(1, allDistances, key=lambda x: weightedSum(weights[i], x[0:6]))
+				scores = map(lambda x: x[6], items)
+				score = sum(scores) / len(items)
+				sseArray[i] += (score - post['realScore']) ** 2
 		print 'done1'
 
-	print sse / len(posts)
+	with open('awesomeresults.txt','w') as f:
+		for i in range(len(weights)):
+			f.write('%s\t%s\n' % (weights[i], sseArray[i] / len(postArray)))
 
