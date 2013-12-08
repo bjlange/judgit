9a711cf1e32f08647c99de39e32719a3bfb6852e
wesleyks <sunwes@gmail.com>
2013-12-07 20:19:41 -0600

    rewrote tester2

--- tester/searcher2.py
+++ tester/searcher2.py
@@ -1,19 +1,25 @@
-import sys
+import sys, os
 from pymongo import MongoClient
 from datetime import datetime, timedelta
-from sklearn.cross_validation import KFold
 sys.path.insert(0, '../learner')
 from regressor import KNearest
 from metrics import get_features
+from heapq import nsmallest
+
+def weightedSum(weights, values):
+	return sum(map(lambda x,y: x*y, weights, values))
 
 if __name__ == '__main__':
-
+	path = os.path.join('data', 'fold1')
+	files = [f for f in os.listdir(path) if os.path.isfile(f)]
 	client = MongoClient()
 	posts = client.reddit.posts
 
 	postArray = []
 
-	for post in posts.find():
+	for f in files:
+		post = posts.find_one({'_id': f})
+
 		scoreToUse = 0
 		submissionTime = datetime.fromtimestamp(post['creation_time'])
 		delta = timedelta(days=1)
@@ -25,31 +31,20 @@ if __name__ == '__main__':
 		post['realScore'] = scoreToUse
 		postArray.append(post)
 
-	kf = KFold(len(postArray), n_folds=30, indices=True, random_state=42)
-
-	trainingIndices = None
-	testingIndices = None
-
-	for train, test in kf:
-		trainingIndices = train
-		testingIndices = test
-
-	trainingData = [postArray[i] for i in trainingIndices]
-	testingData = [postArray[i] for i in testingIndices]
-
-	regressor = KNearest(trainingData)
-
-	#import cProfile
 	print 'starting'
-	with open('results2.txt', 'w') as f:
-		for i in range(3, 6):
-			for val in [0.0, .25, .5, 2.0, 4.0]:
-				weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-				sse = 0
-				weights[i] = val
-				for post in testingData:
-					sse += (regressor.regress(post, 1, weights) - post['realScore']) ** 2
-				mse =  sse / len(testingData)
-				results = 'weights = %s\tk = %s\tmse = %s\n' % (weights, 1, mse)
-				print results
-				f.write(results)
+
+	weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
+	sse = 0.0
+	for post in posts:
+		path = os.path.join('data', 'fold1', post['id'])
+		with open(path, 'r') as f:
+			allDistances = [[float(x) for x in line.split(',')] for line in f.read().split('\n') if len(line) > 0]
+			items = nsmallest(1, allDistances, key=lambda x: weightedSum(weights, x[0:6]))
+			scores = map(lambda x: x[6], items)
+			score = score / len(items)
+			sse += (score - post['realScore']) ** 2
+		print 'done1'
+
+	print sse / len(posts)
+
